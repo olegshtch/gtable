8d03ce1f60c68fc6f802624a121b04f3c0afb489
Oleg Shchelykalnov <olegshtch@yandex.ru>
2010-08-23 15:58:17 +0400

    Remove wrong code

--- src/table_frame.cpp
+++ src/table_frame.cpp
@@ -4,17 +4,10 @@
 
 TableFrame::TableFrame(DB::DataBase &db)
 	:m_DB(db),
-	m_Table(1, 1),
 	m_Auditoriums(ORM::Table::create(DB::g_ModelAud)),
 	m_Days(ORM::Table::create(DB::g_ModelEntity)),
 	m_Hours(ORM::Table::create(DB::g_ModelEntity)),
-	m_LessonRecords(ORM::Table::create(DB::g_ModelLessonRecords)),
-	m_MenuChoose(_("Choose")),
-	m_MenuSet(_("Set")),
-	m_MenuDrop(_("Drop")),
-	m_CellFrom(0),
-	m_CellTo(0),
-	m_CellChoosed(0)
+	m_LessonRecords(ORM::Table::create(DB::g_ModelLessonRecords))
 {
 	pack_start(m_SWTable, true, true);
 	pack_end(m_SWUnsetLessons, false, true);
@@ -22,97 +15,17 @@ TableFrame::TableFrame(DB::DataBase &db)
 	m_DB.ListEntityAud(DB::g_Auditoriums, m_Auditoriums);
 	m_DB.ListEntity(DB::g_Days, m_Days);
 	m_DB.ListEntity(DB::g_Hours, m_Hours);
-	const size_t aud_size = m_Auditoriums->children().size();
-	const size_t days_size = m_Days->children().size();
-	const size_t hours_size = m_Hours->children().size();
-	m_Table.resize(aud_size + 2, days_size * hours_size + 1);
-	
-	// set labels
-	for(size_t a = 0; a < aud_size; a ++)
-	{
-		m_Table.attach(*Gtk::manage(new Gtk::Label(m_Auditoriums->children()[a][DB::g_ModelAud.name])), 2 + a, 3 + a, 0, 1);
-	}
-	for(size_t d = 0; d < days_size; d ++)
-	{
-		for(size_t h = 0; h < hours_size; h ++)
-		{
-			m_Table.attach(*Gtk::manage(new Gtk::Label(m_Hours->children()[h][DB::g_ModelEntity.name])), 1, 2, 1 + hours_size * d + h, 2 + hours_size * d + h);
-			for(size_t a = 0; a < aud_size; a ++)
-			{
-				m_Table.attach(*Gtk::manage(new TableCell(*this, 2 + a, 1 + hours_size * d + h)), 2 + a, 3 + a, 1 + hours_size * d + h, 2 + hours_size * d + h);
-			}
-		}
-		m_Table.attach(*Gtk::manage(new Gtk::Label(m_Days->children()[d][DB::g_ModelEntity.name])), 0, 1, 1 + hours_size * d, 1 + hours_size * (d + 1));
-	}
-
-	/*m_Table.attach(m_TableCell, 2, aud_size + 2, 1, days_size * hours_size + 1);*/
-
-	m_DB.ListLessonRecords(m_LessonRecords);
-	for(Gtk::TreeIter record = m_LessonRecords->children().begin(); record != m_LessonRecords->children().end(); record ++)
-	{
-		m_UnsetLessons.pack_start(*Gtk::manage(new TableCell(
-			*this, 0, 0,
-			record->get_value(DB::g_ModelLessonRecords.t_name),
-			record->get_value(DB::g_ModelLessonRecords.l_name),
-			record->get_value(DB::g_ModelLessonRecords.g_name),
-			record->get_value(DB::g_ModelLessonRecords.hours))),
-			false, true);
-	}
 
 /*	m_UnsetLessons.pack_start(*Gtk::manage(new TableCell("D", "E", "F", 1)));
 	m_UnsetLessons.pack_start(*Gtk::manage(new TableCell("D", "E", "F", 1)));
 	m_UnsetLessons.pack_start(*Gtk::manage(new TableCell("D", "E", "F", 1)));
 	m_UnsetLessons.pack_start(*Gtk::manage(new TableCell("фыфавы", "ыпрварвар", "вролддпд", 3)));*/
 
-	m_SWTable.add(m_Table);
+/*	m_SWTable.add(m_Table);
 	m_SWTable.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
 	m_SWUnsetLessons.add(m_UnsetLessons);
-	m_SWUnsetLessons.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC);
-
-	//create popup menu
-	m_PopupMenu.add(m_MenuChoose);
-	m_MenuChoose.signal_select().connect(sigc::mem_fun(*this, &TableFrame::OnChoose));
-	m_PopupMenu.add(m_MenuSet);
-	m_MenuSet.signal_select().connect(sigc::mem_fun(*this, &TableFrame::OnSet));
-	m_PopupMenu.add(m_MenuDrop);
-	m_MenuDrop.signal_select().connect(sigc::mem_fun(*this, &TableFrame::OnDrop));
-	m_PopupMenu.show_all();
+	m_SWUnsetLessons.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC); */
 
 	show_all();
-}
-
-void TableFrame::OnChoose()
-{
-	if(m_CellChoosed)
-	{
-		m_CellFrom = m_CellChoosed;
-	}
-}
-
-void TableFrame::OnSet()
-{
-	if(m_CellChoosed)
-	{
-		m_CellTo = m_CellChoosed;
-		size_t x = m_CellTo->m_PosX;
-		size_t y = m_CellTo->m_PosY;
-		m_CellFrom->Decrement();
-		m_Table.remove(*m_CellTo);
-		new (m_CellTo) TableCell(*this, x, y, m_CellFrom->GetTeacher(), m_CellFrom->GetLesson(), m_CellFrom->GetGroups(), 1);
-		m_Table.attach(*m_CellTo, x, x + 1, y, y + 1);
-	}
-}
-
-void TableFrame::OnDrop()
-{
-	if(m_CellChoosed)
-	{
-		m_CellTo = m_CellChoosed;
-		size_t x = m_CellTo->m_PosX;
-		size_t y = m_CellTo->m_PosY;
-		m_Table.remove(*m_CellTo);
-		new (m_CellTo) TableCell(*this, x, y);
-		m_Table.attach(*m_CellTo, x, x + 1, y, y + 1);
-	}
 }
 
--- src/table_frame.h
+++ src/table_frame.h
@@ -6,31 +6,20 @@
 #include <gtkmm/scrolledwindow.h>
 #include <gtkmm/menu.h>
 #include "db/db.h"
-#include "table_cell.h"
 
 class TableFrame : public Gtk::HBox
 {
 public:
 	TableFrame(DB::DataBase &db);
 	~TableFrame() {}
-	Gtk::Menu& GetPopupMenu() {return m_PopupMenu;}
-	void SetCell(TableCell *cell) {m_CellChoosed = cell;}
 private:
-	void OnChoose();
-	void OnSet();
-	void OnDrop();
-
 	DB::DataBase &m_DB;
-	Gtk::Table m_Table;
 	Gtk::VBox m_UnsetLessons;
 	Glib::RefPtr<ORM::Table> m_Auditoriums;
 	Glib::RefPtr<ORM::Table> m_Days;
 	Glib::RefPtr<ORM::Table> m_Hours;
 	Gtk::ScrolledWindow m_SWTable, m_SWUnsetLessons;
 	Glib::RefPtr<ORM::Table> m_LessonRecords;
-	Gtk::Menu m_PopupMenu;
-	Gtk::MenuItem m_MenuChoose, m_MenuSet, m_MenuDrop;
-	TableCell *m_CellFrom, *m_CellTo, *m_CellChoosed;
 };
 
 #endif
